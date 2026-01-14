d350b2057b8e093e3c2398581f6ee4c0babaf67b
Timofte Bogdan <Bogdan.Timofte@etas.com>
2026-01-14 11:04:48 +0100

    Fix SharedMemTimeBaseReader_UT.cpp by replicating the ara core usage of custom handler beeing called when abort() function is called

--- src/tsync-utility-lib/test/SharedMemTimeBaseReader_UT/SharedMemTimeBaseReader_UT.cpp
+++ src/tsync-utility-lib/test/SharedMemTimeBaseReader_UT/SharedMemTimeBaseReader_UT.cpp
@@ -7,6 +7,7 @@
 
 #include <cstring>
 #include <mutex>
+#include <csignal>
 
 //!!#include "ara/core/abort.h"
 #include "score/span.hpp"
@@ -43,7 +44,7 @@ protected:
         ON_CALL(*shared_mem_mock, OsClose(::testing::_)).WillByDefault(::testing::Return(0));
 
         // install abort handler for our death tests
-        //!! ara::core::SetAbortHandler(&AbortHandler);
+        signal(SIGABRT, AbortHandler);
         // As we use here singleton mock object, clear expectations after each test
         ::testing::Mock::AllowLeak(shared_mem_mock.get());
     }
@@ -52,11 +53,11 @@ protected:
         tb_reader_.reset();
         shared_mem_mock.reset();
         rw_lock_mock.reset();
-
-        //!! ara::core::SetAbortHandler(nullptr);
+        // use the default abort handler again
+        signal(SIGABRT, SIG_DFL);
     }
 
-    static void AbortHandler() noexcept {
+    static void AbortHandler(int signal) noexcept {
         // the mock has to be reset here, otherwise the expectations for our death tests
         // will never be met/evaluated.
         shared_mem_mock.reset();
